0e2234b15ff6188ec45fba69bf1b8cae4f850a4e
nshuti <nshutikope@gmail.com>
2026-03-18 09:05:05 +0200

    Refactor Main component to include form submission handler and render ingredients dynamically

--- Main.jsx
+++ Main.jsx
@@ -1,26 +1,27 @@
 export default function Main() {
     const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-    
-    /**
-     * Review Challenge:
-     * Map over the list of ingredients and render them as list items
-     * 
-     * Note: We're doing things a weird way here. Don't worry,
-     * we're building up to learning the right way 🙂
-     */
-    
+
+
+    const ingredient = ingredients.map((item) => { return (<li key={item}>{item}</li>) })
+
+    function submitHandler(event) {
+        event.preventDefault()
+        console.log("insert")
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input 
+            <form className="add-ingredient-form" onSubmit={submitHandler}>
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
-                <button>Add ingredient</button>
+                <button >Add ingredient</button>
             </form>
             <ul>
-                {/* Render ingredientsList here */}
+                {ingredient}
             </ul>
         </main>
     )
