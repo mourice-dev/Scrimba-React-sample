767b070efa3ef8a4fbff845f89a80544b3f101da
nshuti <nshutikope@gmail.com>
2026-03-18 16:02:09 +0200

    first commit

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Main from "./Main"
+import List from "./List"
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <List/>
     </>
   )
 }
---
+++ List.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function List() {
+    const [myFavoriteThings, setMyFavoriteThings] = React.useState([])
+
+    const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁",
+        "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
+    const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
+
+    function addFavoriteThing() {
+        setMyFavoriteThings(prevFavThings => [...prevFavThings, "Test"])
+        /**
+         * Challenge: you do it!
+         * Every time "Add item" is clicked, it should add another string
+         * "Test" to the list on the page
+         */
+    }
+
+    return ( 
+        <main>
+            <button onClick={addFavoriteThing}>Add item</button>
+            <section aria-live="polite">
+                {thingsElements}
+            </section>
+        </main>
+    )
+}
